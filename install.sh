442e9a573529216490b95dabf9d17c00a6a50d5a
yurchin23 <61235650+yurchin23@users.noreply.github.com>
2021-04-11 20:30:52 +0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 docker system prune -f --volumes
-docker pull yurchin23/imageapp_jenkins
+docker pull bazhyk/todoappjenkins:latest
 docker-compose -f /home/ubuntu/app/docker-compose.yml up -d
