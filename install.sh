986e15dc5bbb3efdd1ec722ded91d0cbb975458e
yurchin23 <61235650+yurchin23@users.noreply.github.com>
2021-04-11 23:48:58 +0300

    Update install.sh
    finish fix

--- install.sh
+++ install.sh
@@ -5,4 +5,4 @@ sudo apt-get install python3-pip -y
 sudo curl -L "https://github.com/docker/compose/releases/download/1.28.5/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
 sudo chmod +x /usr/local/bin/docker-compose
 sudo usermod -aG docker $USER
-sudo service docker restart
+exit
