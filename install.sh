2865053d8f2f830980e3e05366e5744142933b2e
yurchin23 <61235650+yurchin23@users.noreply.github.com>
2021-04-11 18:40:01 +0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,9 +1,2 @@
 #!/bin/bash
-sudo apt-get update -y
-sudo apt-get install docker.io -y
-sudo apt-get install python3-pip -y
-sudo curl -L "https://github.com/docker/compose/releases/download/1.28.5/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
-sudo chmod +x /usr/local/bin/docker-compose
-sudo usermod -aG docker $USER
-sudo service docker restart
-exit
+docker-compose -f /home/ubuntu/app/docker-compose.yml up -d
